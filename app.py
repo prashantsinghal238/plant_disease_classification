a72454e1ad326c12198c930eccd227e72d9d5d23
Prashant <55804927+prashantsinghal238@users.noreply.github.com>
2021-09-06 18:13:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import os
 import numpy as np
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-from tensorflow.keras.preprocessing import image
-from keras.models import load_model
+# from tensorflow.keras.preprocessing import image
+# from keras.models import load_model
 from keras.models import model_from_json
 app = Flask(__name__)
 potato_labels = ['Early blight','Late blight','Healthy','Bacterial','Black heart','Black scurf','Common scab','Septoria leaf']
@@ -14,88 +14,88 @@ guava_labels = ['Canker','Dot','Mummification','Rust']
 path=os.path.dirname(__file__)
 TARGET_SIZE=(64,64)
 def predict_disease(img_path,label):
-  model_dir = path
-  if label == "tomato":
-    model_json_path = model_dir+"/"+label+"_model.json"
-    model_h5_path = model_dir+"/"+label+"_model.h5"
-    disease_class_labels = tomato_labels
-    img = image.load_img(img_path, target_size=(128,128))
-    img = image.img_to_array(img)
-    img = np.expand_dims(img,axis=0)
-    json_file = open(model_json_path, 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    model = model_from_json(loaded_model_json)
-    model.load_weights(model_h5_path)
-    p = model.predict(img/255.0)
-    v = np.argmax(p)
-  elif label == "potato":
-    model_json_path = model_dir+"/"+label+"_model.json"
-    model_h5_path = model_dir+"/"+label+"_model.h5"
-    disease_class_labels = potato_labels
-    img = image.load_img(img_path, target_size=TARGET_SIZE)
-    img = image.img_to_array(img)
-    img = np.expand_dims(img,axis=0)
-    json_file = open(model_json_path, 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    model = model_from_json(loaded_model_json)
-    model.load_weights(model_h5_path)
-    p = model.predict(img/255.0)
-    v = np.argmax(p)
+  # model_dir = path
+  # if label == "tomato":
+  #   model_json_path = model_dir+"/"+label+"_model.json"
+  #   model_h5_path = model_dir+"/"+label+"_model.h5"
+  #   disease_class_labels = tomato_labels
+  #   img = image.load_img(img_path, target_size=(128,128))
+  #   img = image.img_to_array(img)
+  #   img = np.expand_dims(img,axis=0)
+  #   json_file = open(model_json_path, 'r')
+  #   loaded_model_json = json_file.read()
+  #   json_file.close()
+  #   model = model_from_json(loaded_model_json)
+  #   model.load_weights(model_h5_path)
+  #   p = model.predict(img/255.0)
+  #   v = np.argmax(p)
+  # elif label == "potato":
+  #   model_json_path = model_dir+"/"+label+"_model.json"
+  #   model_h5_path = model_dir+"/"+label+"_model.h5"
+  #   disease_class_labels = potato_labels
+  #   img = image.load_img(img_path, target_size=TARGET_SIZE)
+  #   img = image.img_to_array(img)
+  #   img = np.expand_dims(img,axis=0)
+  #   json_file = open(model_json_path, 'r')
+  #   loaded_model_json = json_file.read()
+  #   json_file.close()
+  #   model = model_from_json(loaded_model_json)
+  #   model.load_weights(model_h5_path)
+  #   p = model.predict(img/255.0)
+  #   v = np.argmax(p)
 
-  elif label == "wheat":
-    model_json_path = model_dir+"/"+label+"_model.json"
-    model_h5_path = model_dir+"/"+label+"_model.h5"
-    disease_class_labels = tomato_labels
-    disease_class_labels = wheat_labels
-    img = image.load_img(img_path, target_size=TARGET_SIZE)
-    img = image.img_to_array(img)
-    img = np.expand_dims(img,axis=0)
-    json_file = open(model_json_path, 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    model = model_from_json(loaded_model_json)
-    model.load_weights(model_h5_path)
-    p = model.predict(img/255.0)
-    v = np.argmax(p)
+  # elif label == "wheat":
+  #   model_json_path = model_dir+"/"+label+"_model.json"
+  #   model_h5_path = model_dir+"/"+label+"_model.h5"
+  #   disease_class_labels = tomato_labels
+  #   disease_class_labels = wheat_labels
+  #   img = image.load_img(img_path, target_size=TARGET_SIZE)
+  #   img = image.img_to_array(img)
+  #   img = np.expand_dims(img,axis=0)
+  #   json_file = open(model_json_path, 'r')
+  #   loaded_model_json = json_file.read()
+  #   json_file.close()
+  #   model = model_from_json(loaded_model_json)
+  #   model.load_weights(model_h5_path)
+  #   p = model.predict(img/255.0)
+  #   v = np.argmax(p)
 
-  elif label == "rice":
-    model_json_path = model_dir+"/"+label+"_model.json"
-    model_h5_path = model_dir+"/"+label+"_model.h5"
-    disease_class_labels = tomato_labels
-    disease_class_labels = rice_labels
-    img = image.load_img(img_path, target_size=TARGET_SIZE)
-    img = image.img_to_array(img)
-    img = np.expand_dims(img,axis=0)
-    json_file = open(model_json_path, 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    model = model_from_json(loaded_model_json)
-    model.load_weights(model_h5_path)
-    p = model.predict(img/255.0)
-    v = np.argmax(p)
+  # elif label == "rice":
+  #   model_json_path = model_dir+"/"+label+"_model.json"
+  #   model_h5_path = model_dir+"/"+label+"_model.h5"
+  #   disease_class_labels = tomato_labels
+  #   disease_class_labels = rice_labels
+  #   img = image.load_img(img_path, target_size=TARGET_SIZE)
+  #   img = image.img_to_array(img)
+  #   img = np.expand_dims(img,axis=0)
+  #   json_file = open(model_json_path, 'r')
+  #   loaded_model_json = json_file.read()
+  #   json_file.close()
+  #   model = model_from_json(loaded_model_json)
+  #   model.load_weights(model_h5_path)
+  #   p = model.predict(img/255.0)
+  #   v = np.argmax(p)
 
-  elif label == "guava":
-    model_json_path = model_dir+"/"+label+"_model.json"
-    model_h5_path = model_dir+"/"+label+"_model.h5"
-    disease_class_labels = tomato_labels
-    disease_class_labels = guava_labels
-    img = image.load_img(img_path, target_size=TARGET_SIZE)
-    img = image.img_to_array(img)
-    img = np.expand_dims(img,axis=0)
-    json_file = open(model_json_path, 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    model = model_from_json(loaded_model_json)
-    model.load_weights(model_h5_path)
-    p = model.predict(img/255.0)
-    v = np.argmax(p)
-  else:
-    return "Invalid Entry"
-  k = disease_class_labels[v]
-  result = str(k)+" : "+str(round(p[0][v]*100,2))+" %"
-  return result
+  # elif label == "guava":
+  #   model_json_path = model_dir+"/"+label+"_model.json"
+  #   model_h5_path = model_dir+"/"+label+"_model.h5"
+  #   disease_class_labels = tomato_labels
+  #   disease_class_labels = guava_labels
+  #   img = image.load_img(img_path, target_size=TARGET_SIZE)
+  #   img = image.img_to_array(img)
+  #   img = np.expand_dims(img,axis=0)
+  #   json_file = open(model_json_path, 'r')
+  #   loaded_model_json = json_file.read()
+  #   json_file.close()
+  #   model = model_from_json(loaded_model_json)
+  #   model.load_weights(model_h5_path)
+  #   p = model.predict(img/255.0)
+  #   v = np.argmax(p)
+  # else:
+  #   return "Invalid Entry"
+  # k = disease_class_labels[v]
+  # result = str(k)+" : "+str(round(p[0][v]*100,2))+" %"
+  return "kam bol chl"
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
